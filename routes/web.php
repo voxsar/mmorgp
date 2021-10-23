e17e21cee53289faea65bca8f535ee29933bba65
vagrant <vagrant@homestead>
2021-10-23 15:16:36 +0000

    search

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Alkhachatryan\LaravelWebConsole\LaravelWebConsole;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    //return LaravelWebConsole::show();
     return view('welcome');
 });
 
